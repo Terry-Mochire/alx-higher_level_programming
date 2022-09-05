135734db8d1f6bb8ec82944805388b521252b348
Terry <terrybmochire@gmail.com>
2022-09-06 01:58:34 +0300

    task 3.

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const numberOfArguments = process.argv.length - 2;
-if (numberOfArguments === 0) {
+const firstArgument = process.argv[2];
+if (firstArgument === undefined) {
   console.log('No argument');
 } else {
-  const firstArgument = process.argv[2];
   console.log(firstArgument);
 }
