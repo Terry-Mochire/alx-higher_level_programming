94327fad18a587f3351040162e9872b28b32cf12
Terry <terrybmochire@gmail.com>
2022-09-11 23:11:55 +0300

    Task 2.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,6 +3,7 @@
 this script takes in an argument and displays all values
 in the states table of hbtn_0e_0_usa where name matches the argument.
 """
+from email import charset
 import sys
 import MySQLdb
 
@@ -14,21 +15,23 @@ if __name__ == "__main__":
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
+        charset="utf8"
     )
 
     # Create a cursor object
     cur = db.cursor()
 
     # Execute a query
-    query = "SELECT * FROM states WHERE name = %s "
-    cur.execute(query, (sys.argv[4],))
+    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    id ASC".format(argv[4]))
 
     # Fetch all the rows in a list of lists
     rows = cur.fetchall()
 
     # Print the rows
     for row in rows:
-        print(row)
+        if row[1] == argv[4]:
+            print(row)
 
     # Close all cursors
     cur.close()
