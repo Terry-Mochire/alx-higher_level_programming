ff04a35103c1622d516393a364468e4dc9461952
Terry <terrybmochire@gmail.com>
2022-10-18 21:47:27 +0300

    Task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
 request(process.argv[2], function (err, response, body) {
-    if (err){
+  if (err) {
     console.log(err);
-    } else {
-    console.log('code:', + response.statusCode);
-    }
+  } else {
+    console.log('code:', +response.statusCode);
+  }
 });
